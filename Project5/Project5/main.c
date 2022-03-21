ea6d394cf3474a4e42eb5cb3bbf21f086545b01f
ssaabbii <82314846+ssaabbii@users.noreply.github.com>
2022-03-21 19:22:50 +0700

    Add files via upload

---
+++ Project5/Project5/main.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "func.h" 
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	int m; double x;
+	scanf("%d\n", &m);
+	for (int i = 0; i < m; i++)
+	{
+		scanf("%lf\n", &x);
+		double eps = 1E-8;
+		double result;
+		if (eps < x && x < 1 - eps)
+			result = ((Function(x + eps) - Function(x - eps)) / 2) / eps;
+		else if (eps >= x)
+			result = (Function(x + eps) - Function(x)) / eps;
+		else if (x >= 1 - eps)
+			result = (Function(x) - Function(x - eps)) / eps;
+		printf("%.15f\n", result);
+	}
+	fclose(stdin);
+	fclose(stdout);
+	return 0;
+}
