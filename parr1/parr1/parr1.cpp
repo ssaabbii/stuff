1cd188b54cd5b1701e35b61b9951609dca48930d
ssaabbii <82314846+ssaabbii@users.noreply.github.com>
2022-03-21 19:27:34 +0700

    Add files via upload

---
+++ parr1/parr1/parr1.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+
+int main()
+{
+	int n = 10000000;
+	double* arr = (double*)malloc(sizeof(double) * n);
+	double sum = 0;
+
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = sin(i * 2 * M_PI / (double)n);
+	}
+
+	for (int i = 0; i < n; i++) {
+		sum += arr[i];
+	}
+
+	printf("%lf\n", sum);
+
+	return 0;
+}
